b83a2e4fb2df439a8dc79f1cbb9c03b5eadcc039
hendorism <hendorism@gmail.com>
2021-02-24 02:04:21 -0800

    finished assignment

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   resources :users
   resources :replies
   resources :posts
+  root 'pages#index'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
